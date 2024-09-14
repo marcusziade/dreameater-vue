b1f90f7d0dc09fdb7af07474f45d7537dac8a6db
Marcus Ziadé <guitaripod@icloud.com>
2024-09-14 10:02:05 +0700

    Update deployment workflow and Vue publicPath configuration

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,7 @@ module.exports = defineConfig({
   transpileDependencies: true
 })
 module.exports = {
-  publicPath: '/'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/dreameater-vue/'
+    : '/'
 }
