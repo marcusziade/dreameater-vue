0c7d1d06c5cb16dcc6a972eb3b1b1ddabddb079e
Marcus Ziadé <guitaripod@icloud.com>
2024-09-14 09:51:58 +0700

    Add deployment workflow for GitHub Pages and set publicPath

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,5 @@ module.exports = defineConfig({
   transpileDependencies: true
 })
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/'
-    : '/'
+  publicPath: '/'
 }
